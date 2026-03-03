2e7c171d3e58a6f4006bee31f9405dcab308204a
bhuvibhatnagar <bhatnagarbhuvi24@gmail.com>
2026-03-03 12:49:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
+require("dotenv").config();
+
 const connectDB = require("./config/db");
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
 
-dotenv.config();
 connectDB();
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Backend is working" });
+  res.json({ message: "Backend is working" });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
