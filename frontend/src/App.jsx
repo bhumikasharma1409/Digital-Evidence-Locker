5339ac9e32197a9365f6861883c706034dffbc27
bhumikasharma1409 <bhumika6432@gmail.com>
2026-03-05 09:12:19 +0530

    Remove unused CaseDetails route from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CitizenDashboard from "./pages/CitizenDashboard";
 import MyCases from "./pages/MyCases";
-import CaseDetails from "./pages/CaseDetails";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<CitizenDashboard />} />
         <Route path="/my-cases" element={<MyCases />} />
-        <Route path="/case/:id" element={<CaseDetails />} />
       </Routes>
     </Router>
   );
